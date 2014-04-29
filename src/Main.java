fb868401d6c1d99f76fd4af4b504c69f7ce993c1
Mads Mortensen <madsxp@gmail.com>
2014-04-29 11:05:35 +0200

    Added some weather data

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+import java.io.IOException;
+
 public class Main {
 
+	public static void main(String[] args) throws IOException {
+		
+		CSVFileReader reader = new CSVFileReader();
+		String[][] data = reader.read("data/weather.csv", false);
+		String[] headers = reader.readHeaders("data/weather.csv");
+		
+	}
 }
